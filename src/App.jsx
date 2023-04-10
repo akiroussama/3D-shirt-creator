ce3887c9d249eeea23aa2729c2c21b05512d04dc
Akir Oussama <akir.oussama@gmail.com>
2023-04-10 11:43:41 +0100

    prepare components and page ( without home )

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 // import canvas, home and costumizer
 import Canvas from './canvas'
-import Home from './pages/Home'
 import Costumizer from './pages/Costumizer'
 
 function App() {
   return (
     <div className="app"> 
-      <Home/>
       <Costumizer/>
       <Canvas/>
     </div>
